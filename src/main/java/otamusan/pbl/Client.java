f559e2cc4b29af8d06504981505035da30244d2f
Otamusan <tamumu.naka@gmail.com>
2020-11-05 08:57:40 +0900

    delete websockets

--- src/main/java/otamusan/pbl/Client.java
+++ src/main/java/otamusan/pbl/Client.java
@@ -1,21 +1,8 @@
 package otamusan.pbl;
 
-import java.io.IOException;
-import java.net.URI;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.websocket.ClientEndpoint;
-import javax.websocket.ContainerProvider;
-import javax.websocket.DeploymentException;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.WebSocketContainer;
-
-@ClientEndpoint
 public class Client {
 	private int delay;
 
@@ -31,54 +18,6 @@ public class Client {
 				Client.this.onUpdate();
 			}
 		}, 1, this.delay);
-
-		// 初期化のため WebSocket コンテナのオブジェクトを取得する
-		WebSocketContainer container = ContainerProvider
-				.getWebSocketContainer();
-		// サーバー・エンドポイントの URI
-		URI uri = URI
-				.create("ws://localhost:9080/hellowebsocket/server");
-		// サーバー・エンドポイントとのセッションを確立する
-		Session session;
-		try {
-			session = container.connectToServer(Client.class,
-					uri);
-			session.getBasicRemote().sendText("こんにちは");
-
-			while (session.isOpen()) {
-				Thread.sleep(100 * 1000);
-				System.err.println("open");
-			}
-		} catch (DeploymentException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		} catch (InterruptedException e) {
-			e.printStackTrace();
-		}
-
-	}
-
-	@OnOpen
-	public void onOpen(Session session) {
-		/* セッション確立時の処理 */
-		System.err.println("[セッション確立]");
-	}
-
-	@OnMessage
-	public void onMessage(String message) {
-		/* メッセージ受信時の処理 */
-		System.err.println("[受信]:" + message);
-	}
-
-	@OnError
-	public void onError(Throwable th) {
-		/* エラー発生時の処理 */
-	}
-
-	@OnClose
-	public void onClose(Session session) {
-		/* セッション解放時の処理 */
 	}
 
 	public void onUpdate() {
--- src/main/java/otamusan/pbl/Server.java
+++ src/main/java/otamusan/pbl/Server.java
@@ -1,24 +1,15 @@
 package otamusan.pbl;
 
-import java.io.IOException;
-import java.util.Set;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.websocket.CloseReason;
-import javax.websocket.EndpointConfig;
-import javax.websocket.OnClose;
-import javax.websocket.OnError;
-import javax.websocket.OnMessage;
-import javax.websocket.OnOpen;
-import javax.websocket.Session;
-import javax.websocket.server.ServerEndpoint;
-@ServerEndpoint("/hellowebsocket")
 public class Server {
 	private int delay;
+
 	public Server(int delay) {
 		this.delay = delay;
 	}
+
 	public void run() {
 		Timer main = new Timer();
 		main.schedule(new TimerTask() {
@@ -26,53 +17,10 @@ public class Server {
 			public void run() {
 				Server.this.onUpdate();
 			}
-		}, 1, delay);
+		}, 1, this.delay);
 	}
+
 	public void onUpdate() {
-		
+
 	}
-	// 現在のセッションを記録
-    Session currentSession = null;
-
-    /*
-     * 接続がオープンしたとき
-     */
-    @OnOpen
-    public void onOpen(Session session, EndpointConfig ec) {
-        this.currentSession = session;
-    }
-
-    /*
-     * メッセージを受信したとき
-     */
-    @OnMessage
-    public void receiveMessage(String msg) throws IOException {
-        // メッセージをクライアントに送信する
-        this.currentSession.getBasicRemote().sendText("Hello " + msg + ".");
-
-        Set<Session> sessions = currentSession.getOpenSessions();
-        for (Session session : sessions) {
-            try {
-                session.getBasicRemote().sendText("Hello " + msg + ".(2)");
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-        }
-    }
-
-    /*
-     * 接続がクローズしたとき
-     */
-    @OnClose
-    public void onClose(Session session, CloseReason reason) {
-        // ...
-    }
-
-    /*
-     * 接続エラーが発生したとき
-     */
-    @OnError
-    public void onError(Throwable t) {
-        // ...
-    }
 }
