63fa6788efea63372895752309c3fc17b6ac6cd2
Otamusan <tamumu.naka@gmail.com>
2020-11-05 09:36:15 +0900

    add sample

--- src/main/java/otamusan/pbl/Client.java
+++ src/main/java/otamusan/pbl/Client.java
@@ -1,5 +1,9 @@
 package otamusan.pbl;
 
+import java.io.IOException;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetSocketAddress;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -12,10 +16,24 @@ public class Client {
 
 	public static void main(String[] args) {
 		Client client = new Client(App.delay);
-		client.run();
+		try {
+			client.run();
+		} catch (IOException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
 	}
 
-	public void run() {
+	public void run() throws IOException {
+
+		String sendData = "UDPてすとですよ";//送信データ
+		byte[] data = sendData.getBytes("UTF-8");//UTF-8バイト配列の作成
+		DatagramSocket sock = new DatagramSocket();//UDP送信用ソケットの構築
+		DatagramPacket packet = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 10005));//指定アドレス、ポートへ送信するパケットを構築
+		sock.send(packet);//パケットの送信
+		System.out.println("UDP送信:" + sendData);//送信データにの表示
+		sock.close();//ソケットのクローズ
+
 		Timer main = new Timer();
 		main.schedule(new TimerTask() {
 			@Override
