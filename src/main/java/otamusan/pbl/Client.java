33e484ddece16a5102e1e78ce8045d7461d68c0c
Otamusan <tamumu.naka@gmail.com>
2020-11-05 09:47:28 +0900

    DatagramChannel sample

--- src/main/java/otamusan/pbl/Client.java
+++ src/main/java/otamusan/pbl/Client.java
@@ -1,9 +1,10 @@
 package otamusan.pbl;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
 import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,13 +27,12 @@ public class Client {
 
 	public void run() throws IOException {
 		System.out.println("クライアント起動");
-		String sendData = "UDPてすとですよ";//送信データ
-		byte[] data = sendData.getBytes("UTF-8");//UTF-8バイト配列の作成
-		DatagramSocket sock = new DatagramSocket();//UDP送信用ソケットの構築
-		DatagramPacket packet = new DatagramPacket(data, data.length, new InetSocketAddress("localhost", 10005));//指定アドレス、ポートへ送信するパケットを構築
-		sock.send(packet);//パケットの送信
-		System.out.println("UDP送信:" + sendData);//送信データにの表示
-		sock.close();//ソケットのクローズ
+		String sendData = "UDPチャネルのてすとですよ";//送信データ
+		DatagramChannel channel = DatagramChannel.open();//DatagramChannelの作成
+		ByteBuffer bb = StandardCharsets.UTF_8.encode(sendData);//送信データをUTF8バイト配列に変換
+		channel.send(bb, new InetSocketAddress("localhost", 10003));//10003へ送信
+		System.out.println("UDPチャネル送信:" + sendData);//送信データの表示
+		channel.close();//チャネルクローズ
 
 		Timer main = new Timer();
 		main.schedule(new TimerTask() {
--- src/main/java/otamusan/pbl/Server.java
+++ src/main/java/otamusan/pbl/Server.java
@@ -1,9 +1,10 @@
 package otamusan.pbl;
 
 import java.io.IOException;
-import java.net.DatagramPacket;
-import java.net.DatagramSocket;
-import java.util.Arrays;
+import java.net.InetSocketAddress;
+import java.nio.ByteBuffer;
+import java.nio.channels.DatagramChannel;
+import java.nio.charset.StandardCharsets;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -26,12 +27,13 @@ public class Server {
 	public void run() throws IOException {
 
 		System.out.println("サーバー起動");
-		DatagramSocket sock = new DatagramSocket(10005);//10005ポートでUDP受信用ソケット構築
-		byte[] data = new byte[1024];//受信最大バッファ
-		DatagramPacket packet = new DatagramPacket(data, data.length);//受信用パケットを構築
-		sock.receive(packet);//受信
-		System.out.println("UDP受信:" + new String(Arrays.copyOf(packet.getData(), packet.getLength()), "UTF-8"));//受信データの表示
-		sock.close();//ソケットのクローズ
+		DatagramChannel channel = DatagramChannel.open();//データグラムチャネルの作成
+		channel.bind(new InetSocketAddress(10003));//受信ポートの指定(10003で受け付け)
+		ByteBuffer bb = ByteBuffer.allocate(1024);//最大1024バイト受信バッファの作成
+		channel.receive(bb);//データの受信
+		bb.flip();//現在のデータを受信データとして確定させる
+		System.out.println("UDPチャネル受信:" + StandardCharsets.UTF_8.decode(bb).toString());//UTF8でバイト配列からStringへ変換
+		channel.close();//チャネルのクローズ
 		Timer main = new Timer();
 		main.schedule(new TimerTask() {
 			@Override
