5023eb5cefc401179f6045eb2f2000f6be638b28
Otamusan <tamumu.naka@gmail.com>
2020-11-07 19:54:56 +0900

    Update Server.java

--- src/main/java/otamusan/pbl/Server.java
+++ src/main/java/otamusan/pbl/Server.java
@@ -4,7 +4,10 @@ import java.io.IOException;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.DatagramChannel;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.Selector;
 import java.nio.charset.StandardCharsets;
+import java.util.Iterator;
 import java.util.Timer;
 import java.util.TimerTask;
 
@@ -27,13 +30,21 @@ public class Server {
 	public void run() throws IOException {
 
 		System.out.println("サーバー起動");
-		DatagramChannel channel = DatagramChannel.open();//データグラムチャネルの作成
-		channel.bind(new InetSocketAddress(10003));//受信ポートの指定(10003で受け付け)
-		ByteBuffer bb = ByteBuffer.allocate(1024);//最大1024バイト受信バッファの作成
-		channel.receive(bb);//データの受信
-		bb.flip();//現在のデータを受信データとして確定させる
-		System.out.println("UDPチャネル受信:" + StandardCharsets.UTF_8.decode(bb).toString());//UTF8でバイト配列からStringへ変換
-		channel.close();//チャネルのクローズ
+		DatagramChannel channel = DatagramChannel.open();
+		channel.bind(new InetSocketAddress(10003));
+		channel.configureBlocking(false);
+		Selector selectorRead = Selector.open();
+		channel.register(selectorRead, SelectionKey.OP_READ);		
+		Iterator<SelectionKey> iterator = selectorRead.selectedKeys().iterator();
+		iterator.forEachRemaining((key)->{
+			key.
+		});
+		ByteBuffer bb = ByteBuffer.allocate(1024);
+		channel.receive(bb);
+		bb.flip();
+		System.out.println("UDPチャネル受信:" + StandardCharsets.UTF_8.decode(bb).toString());
+		channel.close();
+
 		Timer main = new Timer();
 		main.schedule(new TimerTask() {
 			@Override
