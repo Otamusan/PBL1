4d8c818d6f34bdd3b1472764f77b3904dbfaf4d0
Misaki Bear <51944655+MisakiBear@users.noreply.github.com>
2020-11-26 20:18:45 +0900

    Initial Commit

---
+++ TypeInterpreter/DataEncode.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TypeInterpreter
+{
+    public static class DataEncode
+    {
+        public static byte[] GetEncodeData(TypeCode typeCode, object obj)
+        {
+            var processed = $"{typeCode}/{obj}";
+            return Encoding.ASCII.GetBytes(processed);
+        }
+        
+        public static DataRecord GetDecodeData(byte[] bytes)
+        {
+            var decoded = Encoding.ASCII.GetString(bytes);
+            var match = Regex.Match(decoded, @"(.*?)/(.*)");
+            
+            Enum.TryParse(match.Groups[1].Value, out TypeCode typecode);
+            var rawdata = match.Groups[2].Value;
+
+            return new DataRecord
+            {
+                TypeCode = typecode,
+                RawData = rawdata,
+            };
+        }
+    }
+}
---
+++ TypeInterpreter/DataRecord.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace TypeInterpreter
+{
+    public record DataRecord
+    {
+        public TypeCode TypeCode { get; init; }
+        public string RawData { get; init; }
+    }
+}
